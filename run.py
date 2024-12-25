851bdcf00ac062989d2bec0a989369640e264d03
rulercosta <rulercosta.mail@gmail.com>
2024-12-26 00:59:49 +0530

    Fix run debug

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    debug_mode = os.getenv("FLASK_ENV") == "development"
+    print(f"Starting application with debug mode set to {debug_mode}")
+    app.run(debug=debug_mode)
