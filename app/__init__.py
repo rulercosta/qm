a1c9edd0db5f529799280a2a10f30e8abd512063
rulercosta <rulercosta.mail@gmail.com>
2025-02-11 02:32:25 +0530

    Remove admin functionality and related routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,16 @@
 import os
 from flask import Flask
-from app.extensions import db, session, admin, login_manager, bcrypt
-from app.routes import home_routes, verify_routes, contact_routes, admin_routes, events_routes, explore_routes
+from app.extensions import db, session
+from app.routes import home_routes, verify_routes, contact_routes, events_routes, explore_routes
 from app.config import Config
-from app.routes.auth_routes import auth_bp
-from app.routes.admin_routes import SecureAdminIndexView
 
 def create_app():
     app = Flask(__name__, template_folder=os.path.join(os.getcwd(), 'templates'), static_folder=os.path.join(os.getcwd(), 'static'))
     app.config.from_object(Config)
-    
-    # Add custom admin static files configuration
-    app.config['FLASK_ADMIN_SWATCH'] = 'slate'
-    app.config['FLASK_ADMIN_FLUID_LAYOUT'] = True
-    app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
     # Initialize extensions
     db.init_app(app)
     session.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-    
-    # Initialize admin with secure view
-    admin.init_app(app)
-    admin.name = 'QM Admin'
-    admin.index_view = SecureAdminIndexView()
-    # Remove custom template settings to use default Flask-Admin templates
-
-    # Initialize admin views
-    from app.routes.admin_routes import init_admin_views
-    with app.app_context():
-        init_admin_views()
 
     # Register blueprints
     app.register_blueprint(home_routes.bp)
@@ -38,8 +18,6 @@ def create_app():
     app.register_blueprint(verify_routes.bp)
     app.register_blueprint(contact_routes.bp)
     app.register_blueprint(explore_routes.bp)
-    app.register_blueprint(admin_routes.bp)
-    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
 
--- app/extensions.py
+++ app/extensions.py
@@ -1,22 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
-from flask_admin import Admin
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 session = Session()
-login_manager = LoginManager()
-login_manager.login_view = "auth.login"  
-bcrypt = Bcrypt()
-
-# Simplify admin configuration
-admin = Admin(
-    name='QM Admin',
-    template_mode='bootstrap3'
-)
-
-@login_manager.user_loader
-def load_user(user_id):
-    from app.models import AdminUser
-    return AdminUser.query.get(int(user_id))
