a89b216778d65ba68e89692a381ca96b06f67ef8
rulercosta <rulercosta.mail@gmail.com>
2025-02-18 05:28:35 +0530

    Refactor configuration management to centralize database URL and session settings

--- app/configs/config.py
+++ app/configs/config.py
@@ -1,25 +1,38 @@
 import os
-from dotenv import load_dotenv
+from app.utils.env_loader import get_project_root
+from app.utils.settings import settings
 
-load_dotenv()
+def get_database_url(flask_env):
+    """Centralized database URL configuration"""
+    if flask_env == 'development':
+        db_path = os.path.join(get_project_root(), 'db.sqlite3')
+        return f'sqlite:///{db_path}'
+    return os.getenv('DATABASE_URL')
 
 class BaseConfig:
     SECRET_KEY = os.getenv('SESSION_SECRET_KEY', 'default-dev-key')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SESSION_TYPE = 'filesystem'
-    SESSION_FILE_DIR = os.path.join(os.getcwd(), 'flask_session')
+    SESSION_FILE_DIR = os.path.join(settings.root_path, 'flask_session')
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
+    
+    # Use settings for database configuration
+    SQLALCHEMY_DATABASE_URI = settings.db_url
     SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_size': int(os.getenv('DB_POOL_SIZE', '100')),
-        'max_overflow': int(os.getenv('DB_MAX_OVERFLOW', '50')),
-        'pool_timeout': int(os.getenv('DB_POOL_TIMEOUT', '30')),
-        'pool_recycle': int(os.getenv('DB_POOL_RECYCLE', '1800')),
+        'pool_size': settings.db_pool_size,
+        'max_overflow': settings.db_max_overflow,
+        'pool_timeout': settings.db_pool_timeout,
+        'pool_recycle': settings.db_pool_recycle,
         'pool_pre_ping': True
     }
     SQLALCHEMY_POOL_PRE_PING = True
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+
+    @classmethod
+    def init_app(cls, flask_env):
+        cls.SQLALCHEMY_DATABASE_URI = get_database_url(flask_env)
+        return cls
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
@@ -36,3 +49,8 @@ config = {
     'testing': TestingConfig,
     'default': DevelopmentConfig
 }
+
+def get_config(flask_env):
+    """Get configuration based on environment"""
+    config_class = config.get(flask_env, config['default'])
+    return config_class.init_app(flask_env)
